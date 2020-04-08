bf741ed2eb5fabfe727cdd0bd17465861feaa78e
AntonioSkopin <askopin@outlook.com>
2020-04-08 17:34:01 +0200

    Updates

---
+++ precache-manifest.50662055d1b6571b6b36009f236b038d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4d8dccce27d5c278ac3945ef5525a9",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "c8f3c822a4a775d0de5b",
+    "url": "/react-calculator/static/css/main.46374f2e.chunk.css"
+  },
+  {
+    "revision": "0cf84e9f1f6ca4fbb44b",
+    "url": "/react-calculator/static/js/2.fec65fd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.fec65fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f3c822a4a775d0de5b",
+    "url": "/react-calculator/static/js/main.244e3fc0.chunk.js"
+  },
+  {
+    "revision": "8df0bcfbb2330658b1c5",
+    "url": "/react-calculator/static/js/runtime-main.2eff8855.js"
+  }
+]);
